1ef52600b712d18e30d9f0d9297afad3085b9d01
Zubee74 <you@example.com>
2023-01-04 15:47:43 +0530

    OrangeHRM_info dashboard

---
+++ tests/HRM_info.spec.js
@@ -0,0 +1,47 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByPlaceholder('Username').fill('Admin');
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await page.getByRole('link', { name: 'My Info' }).click();
+  await page.getByPlaceholder('First Name').click();
+  await page.getByPlaceholder('First Name').fill('Mohammad ');
+  await page.getByPlaceholder('Middle Name').click();
+  await page.getByPlaceholder('Middle Name').fill('Zubair');
+  await page.getByPlaceholder('Last Name').click();
+  await page.getByPlaceholder('Last Name').fill('Tester');
+  await page.locator('.oxd-grid-item > .oxd-input-group > div:nth-child(2) > .oxd-input').first().click();
+  await page.locator('.oxd-grid-item > .oxd-input-group > div:nth-child(2) > .oxd-input').first().fill('Zubee');
+  await page.locator('div:nth-child(3) > div > div > .oxd-input-group > div:nth-child(2) > .oxd-input').first().click();
+  await page.locator('div:nth-child(3) > div > div > .oxd-input-group > div:nth-child(2) > .oxd-input').first().fill('0025');
+  await page.locator('.oxd-grid-3 > div:nth-child(2) > .oxd-input-group > div:nth-child(2) > .oxd-input').first().click();
+  await page.locator('.oxd-grid-3 > div:nth-child(2) > .oxd-input-group > div:nth-child(2) > .oxd-input').first().fill('007');
+  await page.locator('div:nth-child(3) > div:nth-child(2) > div > .oxd-input-group > div:nth-child(2) > .oxd-input').click();
+  await page.locator('div:nth-child(3) > div:nth-child(2) > div > .oxd-input-group > div:nth-child(2) > .oxd-input').fill('9766144');
+  await page.locator('form').filter({ hasText: 'Employee Full NameNicknameEmployee IdOther IdDriver\'s License NumberLicense Expi' }).locator('i').first().click();
+  await page.getByPlaceholder('yyyy-mm-dd').first().click();
+  await page.getByPlaceholder('yyyy-mm-dd').first().fill('1997-11-11');
+  await page.locator('div:nth-child(3) > div > .oxd-input-group > div:nth-child(2) > .oxd-input').first().click();
+  await page.locator('div:nth-child(3) > div > .oxd-input-group > div:nth-child(2) > .oxd-input').first().fill('00001');
+  await page.locator('div:nth-child(3) > div:nth-child(2) > .oxd-input-group > div:nth-child(2) > .oxd-input').click();
+  await page.locator('div:nth-child(3) > div:nth-child(2) > .oxd-input-group > div:nth-child(2) > .oxd-input').fill('00002');
+  await page.locator('form').filter({ hasText: 'Employee Full NameNicknameEmployee IdOther IdDriver\'s License NumberLicense Expi' }).locator('i').nth(1).click();
+  await page.getByText('Indian').click();
+  await page.locator('div:nth-child(2) > .oxd-input-group > div:nth-child(2) > .oxd-select-wrapper > .oxd-select-text').click();
+  await page.getByRole('option', { name: 'Single' }).click();
+  await page.getByPlaceholder('yyyy-mm-dd').nth(1).click();
+  await page.getByPlaceholder('yyyy-mm-dd').nth(1).fill('1975-10-11');
+  await page.getByPlaceholder('yyyy-mm-dd').nth(1).click();
+  await page.getByPlaceholder('yyyy-mm-dd').nth(1).fill('1996-11-12');
+  await page.getByText('Male').first().click();
+  await page.locator('div:nth-child(7) > .oxd-grid-3 > div > .oxd-input-group > div:nth-child(2) > .oxd-input').click();
+  await page.locator('div:nth-child(7) > .oxd-grid-3 > div > .oxd-input-group > div:nth-child(2) > .oxd-input').fill('NA');
+  await page.locator('label').filter({ hasText: 'Yes' }).locator('i').click();
+  await page.locator('form').filter({ hasText: 'Employee Full NameNicknameEmployee IdOther IdDriver\'s License NumberLicense Expi' }).getByRole('button', { name: 'Save' }).click();
+  await page.getByRole('paragraph').filter({ hasText: 'Paul Collings' }).click();
+  await page.getByRole('menuitem', { name: 'Logout' }).click();
+});
